1ab8edb70ce2905f0ca1b9328256b6ae8b085d65
Paul Vorbach <paul@vorba.ch>
2014-01-02 16:51:24 +0100

    Utility methods for Color

--- src/main/scala/de/vorb/depict/tracing/Color.scala
+++ src/main/scala/de/vorb/depict/tracing/Color.scala
@@ -1,6 +1,11 @@
 package de.vorb.depict.tracing
 
-class Color(val argb: Int) extends AnyVal
+class Color(val argb: Int) extends AnyVal {
+  def isBlack = argb == 0xFF000000
+  def isWhite = argb == 0xFFFFFFFF
+  def isBinary = isBlack || isWhite
+  def isNonBinary = !isBinary
+}
 
 object Color {
   def apply(argb: Int) = new Color(argb)

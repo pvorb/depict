144f89bc5789f74538fe69a6279a484fe8f02842
Paul Vorbach <paul@vorba.ch>
2014-01-03 17:25:05 +0100

    Find all paths

--- src/main/scala/de/vorb/depict/Potrace.scala
+++ src/main/scala/de/vorb/depict/Potrace.scala
@@ -3,21 +3,20 @@ package de.vorb.depict
 import scala.collection.immutable.VectorBuilder
 import TurnDirection.Left
 import TurnDirection.Right
+import scala.collection.immutable.VectorBuilder
 
 object Potrace {
-
   /**
    * Finds the next starting point (i.e. black pixel) starting at `from`.
    */
-  def nextStartingPoint(start: Point[Int])(img: Bitmap): Option[Point[Int]] = {
-    val Point(x0, y0) = start
+  def findNext(start: Point[Int])(implicit img: Bitmap): Option[Point[Int]] = {
+    var x = start.x
+    var y = start.y
 
-    var x = x0
-    var y = y0
     while (y < img.height) {
       while (x < img.width) {
-        if (img(x, y).isWhite && img(x, y).isBlack) // found next black
-          return Some(Point(x - 1, y))
+        if (img(x - 1, y).isWhite && img(x, y).isBlack) // found next black
+          return Some(Point(x, y))
 
         x += 1
       }
@@ -29,7 +28,12 @@ object Potrace {
     None
   }
 
-  def getPath(start: Point[Int], turnPolicy: TurnPolicy = TurnPolicy.Left)(implicit img: Bitmap): Vector[Point[Int]] = {
+  /**
+   * Returns a closed path beginning at the given starting point.
+   */
+  def findPath(start: Point[Int], turnPolicy: TurnPolicy = TurnPolicy.Left)(
+    implicit img: Bitmap): Path = {
+
     import TurnDirection._
 
     val path = new VectorBuilder[Point[Int]]
@@ -42,23 +46,20 @@ object Potrace {
       path += edge.to
 
       val next = edge.next()
-      if (next.isValid) {
+      if (next.isValidEdge) {
         edge = next
       } else {
         val left = edge.next(Some(Left))
         val right = edge.next(Some(Right))
 
-        // TODO
-        println(edge, left, right)
-
-        if (left.isValid && right.isValid) {
+        if (left.isValidEdge && right.isValidEdge) {
           turnPolicy.turnDirection(edge) match {
             case TurnDirection.Left =>
               edge = left
             case TurnDirection.Right =>
               edge = right
           }
-        } else if (left.isValid) {
+        } else if (left.isValidEdge) {
           edge = left
         } else {
           edge = right
@@ -66,6 +67,28 @@ object Potrace {
       }
     }
 
-    path.result
+    path += start // closed path
+
+    Path(path.result)
+  }
+
+  def findAllPaths(turdSize: Int = 2)(implicit img: Bitmap): Vector[Path] = {
+    var ps = new VectorBuilder[Path]
+
+    var line = 0
+    val height = img.height
+    var startPoint = findNext(Point(0, 0))
+    while (startPoint != None) {
+      val path = findPath(startPoint.get)
+
+      // omit paths that have less than `turdSize` pixels
+      if (path.area >= turdSize)
+        ps += path
+
+      path.invert
+      startPoint = findNext(startPoint.get)
+    }
+
+    ps.result
   }
 }
